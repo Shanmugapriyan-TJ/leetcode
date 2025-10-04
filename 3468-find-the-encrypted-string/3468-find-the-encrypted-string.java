c55b810820e121f044234ecc45a0aebb84f4394b
Shanmugapriyan-TJ <121568453+Shanmugapriyan-TJ@users.noreply.github.com>
2025-10-04 08:48:37 +0530

    Time: 1 ms (100%), Space: 42.2 MB (99.48%) - LeetHub

---
+++ 3468-find-the-encrypted-string/3468-find-the-encrypted-string.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String getEncryptedString(String s, int k) {
+        int n = s.length();
+        
+        k %= n;
+        
+        return s.substring(k) + s.substring(0, k);
+    }
+}
