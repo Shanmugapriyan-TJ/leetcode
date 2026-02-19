24c629ee5cc0d59b588d0d37e51fd5161c0b2c22
Shanmugapriyan-TJ <121568453+Shanmugapriyan-TJ@users.noreply.github.com>
2026-02-19 11:17:16 +0530

    Time: 6 ms (89.17%), Space: 19.5 MB (19.25%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,28 +1,23 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        if numRows==1: return s
+        if numRows == 1:
+            return s
 
-        matrix=[[0]*len(s) for i in range(numRows)] # Matrix
-        i,r,c=0,0,0
+        mat = [[] for _ in range(numRows)]
+        i = 0
+        n = len(s)
 
-        # Build the structure
-        while i<len(s):
-            if r==0:
-                while r<numRows and i<len(s):
-                    matrix[r][c]=s[i]
-                    r+=1
-                    i+=1
-                r-=2 # Reset the pointer for diagonal
-            else:
-                matrix[r][c]=s[i]
-                r-=1
-                i+=1
-            c+=1
+        while i < n:
+            for down in range(numRows):
+                if i < n:
+                    mat[down].append(s[i])
+                    i += 1
+            for up in range(numRows - 2, 0, -1):
+                if i < n:
+                    mat[up].append(s[i])
+                    i += 1
 
-        # Add all the values to ans string, row by row
-        ans=''
-        for i in range(numRows):
-            for j in range(len(s)):
-                if matrix[i][j]!=0:
-                    ans+=matrix[i][j]
+        ans = ""
+        for row in mat:
+            ans += ''.join(row)
         return ans
